6d739f581f91c465a503a3a35ce41afb102ea20d
B0T-Eric <115904192+B0T-Eric@users.noreply.github.com>
2024-08-04 19:39:21 +1200

    Add Round Button

--- Mobile App/ArcheryProjectApp/ArcheryProjectApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ArcheryProjectApp.AssemblyInfo.cs
+++ Mobile App/ArcheryProjectApp/ArcheryProjectApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ArcheryProjectApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArcheryProjectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f042aad86cd82a30afb967c71a45ae564b58d455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e5632a00666dc5045f5aa0a33a09e84b71f4b8b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArcheryProjectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArcheryProjectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
